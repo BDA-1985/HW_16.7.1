e878686eec3d595fe5e7b0f2ebac2a5743d4f710
Dmitriy Borodin <dimon_alf@mail.ru>
2023-02-28 17:49:38 +0300

    Задание 16.7.1 Откройте сайт «Дом питомца» и на основе имеющихся в нем данных создайте конструктор класса Cat со следующими параметрами: имя, пол, возраст.
    В отдельный файл импортируйте и создайте объект Cat, который выводит имеющихся на сайте котов, с одинаковыми параметрами, но с разными значениями.

---
+++ Inf_about_cat.py
@@ -0,0 +1,7 @@
+from main import Cat
+
+c1 = Cat("Fluffy", "m", 2)
+c2 = Cat("Mony", "g", 1)
+
+print(F'I am Cat. My name is {c1.name} with gender {c1.gender} and age {c1.age}')
+print(F'I am Cat. My name is {c2.name} with gender {c2.gender} and age {c2.age}')
